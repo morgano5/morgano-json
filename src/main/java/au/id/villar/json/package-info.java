d590506f8bc0b7b71d91ad4ca6707a8dc4b81f5a
Rafael Villar Villar <morgano5@gmail.com>
2013-07-02 12:56:57 +1000

    Add javadoc to package au.id.villar.json

--- src/main/java/au/id/villar/json/package-info.java
+++ src/main/java/au/id/villar/json/package-info.java
@@ -18,6 +18,30 @@ along with Morgano-json.  If not, see <http://www.gnu.org/licenses/>.
 /**
  * <p>Contains utilities to work with JSON code. These classes allow to parse JSON code from a java.io.Reader to get,
  * java objects and get JSON code from java objects.</p>
- * 
+ *
+ * <p>To parse a JSON stream in an analog way to XML SAX:</p>
+ * <ul>
+ *     <li>Implement a {@link au.id.villar.json.ContentHandler} and an {@link au.id.villar.json.ErrorHandler}
+ *     (see documentation for these interfaces).</li>
+ *     <li>Create an instance of {@link au.id.villar.json.JSONReader}.</li>
+ *     <li>Use the implementations of first step and the input (must be a {@link java.io.Reader}) to set:
+ *      <ul>
+ *          <li>contentHandler</li>
+ *          <li>errorHandler</li>
+ *          <li>input</li>
+ *      </ul>
+ *     </li>
+ *     <li>Invoke parse() in the JSONReader object</li>
+ * </ul>
+ *
+ * <p>To get an object serialized and written in a {@link java.io.Writer},
+ * use {@link au.id.villar.json.ObjectSerializer#write(Object, java.io.Writer)}</p>
+ *
+ * <p>To get an object deserialized from a {@link java.io.Reader}, use one of these static methods:</p>
+ * <ul>
+ *     <li>{@link au.id.villar.json.ObjectDeserializer#getFromReader(java.io.Reader)}</li>
+ *     <li>{@link au.id.villar.json.ObjectDeserializer#getFromReader(java.io.Reader, Class)}</li>
+ *     <li>{@link au.id.villar.json.ObjectDeserializer#internalGetFromReader(java.io.Reader, Object)}</li>
+ * </ul>
  */
 package au.id.villar.json;
