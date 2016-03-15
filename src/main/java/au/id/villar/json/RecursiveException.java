32c37fa3a5cef5d2bbf46ea1f576897bb5120f3b
Rafael Villar Villar <rafael@villar.me>
2016-03-16 06:13:42 +1000

    Add some documentation

--- src/main/java/au/id/villar/json/RecursiveException.java
+++ src/main/java/au/id/villar/json/RecursiveException.java
@@ -1,24 +1,24 @@
 /*
-Copyright (c) 2012 Rafael Villar Villar
+ * Morgano-json library to convert between POJOs and JSON
+ * Copyright (c) 2016 Rafael Villar Villar
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-This file is part of Morgano-json.
-
-Morgano-json is free software: you can redistribute it and/or modify it under
-the terms of the GNU Lesser General Public License as published by the Free
-Software Foundation, either version 3 of the License, or any later version.
-
-Morgano-json is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with Morgano-json.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 package au.id.villar.json;
 
 public class RecursiveException extends RuntimeException {
-
-	private static final long serialVersionUID = -728154219849101547L;
 
 }
